a858602c952cf36cf6f8b7cbcf3dd688cdce36b0
Ilia <isinetov@mail.ru>
2024-05-09 18:07:19 +0400

    ЗАДАНИЕ 2. Напишите программу, которая считает размер папки на диске (вместе со всеми вложенными папками и файлами). На вход метод принимает URL директории, в ответ — размер в байтах.

--- Task2/Task2.cs
+++ Task2/Task2.cs
@@ -1,16 +1,61 @@
 ﻿class Task2
 {
-    static void Main(string[] args)
+    static long GetDirectorySize(string PATH)
     {
-        Console.Write("Введите путь к папке: ");
-        string PATH = Console.ReadLine();
+        long dirSize = 0;
+
         if (Directory.Exists(PATH))
         {
+            var directory = new DirectoryInfo(PATH);
+            var directories = directory.GetDirectories();
+            var files = directory.GetFiles();
 
+            if (files != null)
+            {
+                foreach (var file in files)
+                {
+                    try 
+                    {
+                        dirSize += file.Length;
+                    }
+                    catch (UnauthorizedAccessException) 
+                    {
+                        Console.WriteLine($"Ошибка: Системе не удалось получить доступ к файлу {file.Name}!!!");
+                    }
+                }
+
+                foreach (var dir in directories)
+                {
+                    try 
+                    {
+                        dirSize += GetDirectorySize(dir.FullName);
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+                        Console.WriteLine($"Ошибка: Системе не удалось получить доступ к папке {dir.Name}!!!");
+                    }
+                }
+                return dirSize;
+            }
+
+            return dirSize;
         }
+
         else
         {
             Console.WriteLine("Ошибка: Папки по такому пути не существует!!!");
+            return dirSize;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        while (true) 
+        {
+            Console.Write("Введите путь к папке: ");
+            string PATH = Console.ReadLine();
+            var fileSize = GetDirectorySize(PATH);
+            Console.WriteLine($"Размер папки по указанному пути равен: {fileSize} байт");
         }
     }
 }
