7a6224042626dbb41eb05ddb23c1f9bcda62d24c
Ilia <isinetov@mail.ru>
2024-05-09 15:58:20 +0400

    ЗАДАНИЕ 1. Напишите программу, которая чистит нужную нам папку от файлов  и папок, которые не использовались более 30 минут .

---
+++ Task_1/Task1.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var timeCheck = TimeSpan.FromMinutes(30);
+
+        while (true)
+        {
+            Console.Write("Введите путь к папке: ");
+            string PATH = Console.ReadLine();
+
+            if (Directory.Exists(PATH))
+            {
+                DirectoryInfo mainDirectory = new DirectoryInfo(PATH);
+
+                var directories = mainDirectory.GetDirectories();
+                var files = mainDirectory.GetFiles();
+
+                foreach (var directory in directories)
+                {
+                    try
+                    {
+                        var timeUnused = DateTime.Now - directory.LastAccessTime;
+                        if (timeUnused > timeCheck)
+                        {
+                            directory.Delete(true);
+                        }
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+                        Console.WriteLine($"Ошибка: Системе не удалось получить доступ к папке {directory.Name}!!!");
+                    }
+                }
+
+                foreach (var file in files)
+                {
+                    try
+                    {
+                        var timeUnused = DateTime.Now - file.LastAccessTime;
+                        if (timeUnused > timeCheck)
+                        {
+                            file.Delete();
+                        }
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+                        Console.WriteLine($"Ошибка: Системе не удалось получить доступ к файлу {file.Name}!!!");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: Папки по такому пути не существует!!!");
+            }
+        }
+    }
+}
